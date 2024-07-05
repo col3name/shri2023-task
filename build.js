3e7b8f65c0328862250f1fa359eec621bb5e5c08
col3name <mmikusho@instaloper.com>
2024-07-05 22:50:13 +0300

    feat: minify js and css

--- build.js
+++ build.js
@@ -1,12 +1,41 @@
 const fs = require('node:fs');
-const babel = require('./vendors/babel.min');
+const babel = require('./vendors/babel.min.js');
+const { exec } = require('child_process');
+
+
 const sourceFile = './src/App.jsx';
+
 const content = fs.readFileSync(sourceFile, { type: ''});
 
-console.log({content})
+// console.log({content})
 const result = babel.transform(content, {
     presets: ['react', 'es2017']
 })
 
 fs.writeFileSync('app.build.js', result.code);
+
+
+function execute(command) {
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            return;
+        }
+
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
+}
+
+function uglifyJs(inputPath, outputPath) {
+    execute(`uglifyjs --compress --mangle -- ${inputPath} > ${outputPath}`);
+}
+
+function minify(inputPath, outputPath) {
+    execute(`minify ${inputPath} > ${outputPath}`);
+
+}
+
+uglifyJs('vendors/react-with-dom.js', 'vendors/react-with-dom.min.js');
+uglifyJs('app.build.js', 'app.build.min.js');
+minify('styles.css', 'styles.min.css');
 
